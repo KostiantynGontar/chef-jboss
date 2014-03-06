58e56ef9750a1ec12b20e599d56cd63e0de06c4a
Kostiantyn Gontar <kostiantyn_gontar@epam.com>
2014-03-06 19:31:44 +0200

    Change units and add integration tests

--- spec/units/recipes/default_spec.rb
+++ spec/units/recipes/default_spec.rb
@@ -1,23 +1,18 @@
-require_relative '../spec_helper'
+require 'chefspec'
+require 'chefspec/berkshelf'
 
 describe 'jboss::default' do
 
   let(:chef_run) { ChefSpec::Runner.new.converge(described_recipe) }
 
   it 'set jboss attributes' do
-    expect(chef_run.node['jboss_home']).to eq('/opt/jboss')
     expect(chef_run.node['jboss']['user']).to eq('jboss')
-    expect(chef_run.node['jboss_version']).to eq('7.1.1')
   end
 
   it 'creates a user' do
     expect(chef_run).to create_user('jboss')
   end 
 
-  it 'install jboss with Ark' do
-    expect(chef_run).to create_directory('/opt/jboss').with(user:'jboss')
-  end
-    
   it 'create init script' do
     expect(chef_run).to create_link('/etc/init.d/jboss').with(to:'/opt/jboss/bin/standalone.sh')
   end
